a35af914cf0a2c686831af1d232b488d562132da
armankhoshnevis <khoshne1@msu.edu>
2026-04-07 23:43:12 -0400

    Add job scripts

---
+++ scripts/LSA/lsa.sh
@@ -0,0 +1,29 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=LSA_FMG
+
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --time=01:00:00
+
+# Load necessary modules (Adapt these to your specific cluster environment)
+ml purge
+ml load Miniforge3
+ml load powertools
+
+# Activate the conda environment (Make sure the Conda installation guide is followed)
+conda activate SA_Project
+
+# Print job information
+echo "Job started at $(date)"
+echo "Running on host $(hostname)"
+
+# Run the Python script
+# Note: Ensure you are in the correct directory or provide absolute paths
+python fmg_main.py --HS 20 --GnP_idx 0 --n_mc 100000 --batch_size 50000
+
+echo "Job finished at $(date)"
